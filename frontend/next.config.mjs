0f96fcb22ca83bd57595594211c29dafb9673c55
ivaylo2000 <ivaylo07082000@gmail.com>
2024-09-16 01:55:39 +0300

    changes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["via.placeholder.com", "localhost"],
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
