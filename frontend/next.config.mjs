981ff844a661e13e146be490b49fab9f456d2a27
ivaylo2000 <ivaylo07082000@gmail.com>
2024-09-16 01:10:44 +0300

    c

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
       "via.placeholder.com",
       "localhost",
       process.env.NEXT_PUBLIC_IMAGE_DOMAIN,
-    ], // Add other domains if needed
+    ],
   },
   webpack(config) {
     config.module.rules.push({
