86c86fd2b581770d1056c34299816e4c389b9028
ivaylo2000 <ivaylo07082000@gmail.com>
2024-09-16 01:06:07 +0300

    small change

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["via.placeholder.com", "localhost"], // Add other domains if needed
+    domains: [
+      "via.placeholder.com",
+      "localhost",
+      process.env.NEXT_PUBLIC_IMAGE_DOMAIN,
+    ], // Add other domains if needed
   },
   webpack(config) {
     config.module.rules.push({
